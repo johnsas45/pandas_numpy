c9f5c154ba4cde9bd70da81b31dad22ddf285862
johnny sanchez <johnsas45@gmail.com>
2024-08-03 14:25:32 -0500

    abordamos tipos de datos en numpy

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+# arr = np.array([1,2,3,4], dtype='float64')
+# print(arr)
+# arr.dtype
+# print(arr.dtype)
+# arr = np.array([1,2,3,4])
+# print(arr)
+# arr = arr.astype(np.float64)
+# print(arr)
+# arr = np.array([0,1,2,3,4])
+# arr=arr.astype(np.bool_)
+# print(arr)
+# arr = np.array([0,1,2,3,4])
+# arr=arr.astype(np.str)
+# print(arr)
+arr = np.array(['0','1','2','3','4'])
+arr = arr.astype(np.int8)
+print(arr)
